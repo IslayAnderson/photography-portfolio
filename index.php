acaad8932d9904b068aed23898b3feef377e0946
Islay Anderson <islayanderson@outlook.com>
2024-09-14 23:36:57 +0100

    convert to svg

---
+++ index.php
@@ -0,0 +1,75 @@
+
+<!doctype html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Islay Anderson photography portfolio</title>
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    <meta name="viewport" content="width=600, height=800" />
+    <meta name="robots" content="index, follow, max-image-preview:large, max-snippet:-1, max-video-preview:-1" />
+    <link rel="canonical" href="https://photography.islayanderson.co.uk/" />
+    <link rel="stylesheet" href="/styles/css/main.css">
+    <meta property="og:locale" content="en_US" />
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="Photography - Islay Anderson photography portfolio" />
+    <meta property="og:url" content="https://photography.islayanderson.co.uk/" />
+    <meta property="og:site_name" content="Islay Anderson photography portfolio" />
+    <meta property="article:modified_time" content="2024-09-01T18:15:46+00:00" /> <!-- KEEP UPDATED -->
+    <meta property="og:image" content="./images/IMG_7006-679x1024.jpg" /> <!-- TODO: UPDATE ME -->
+    <meta name="twitter:card" content="summary_large_image" />
+    <meta name="twitter:site" content="@islayAnderson" />
+    <meta name="twitter:label1" content="Est. reading time" />
+    <meta name="twitter:data1" content="14 minutes" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Anton&family=Roboto+Condensed:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="dns-prefetch" href="//www.googletagmanager.com" />
+    <link href="https://photography.islayanderson.co.uk" rel="preconnect" />
+    <script type="text/javascript" src="https://www.googletagmanager.com/gtag/js?id=GT-WRDFXCC8" id="google_gtagjs-js"
+        async></script>
+    <script type="text/javascript" id="google_gtagjs-js-after">
+        /* <![CDATA[ */
+        window.dataLayer = window.dataLayer || []; function gtag() { dataLayer.push(arguments); }
+        gtag("set", "linker", { "domains": ["blog.islayanderson.co.uk"] });
+        gtag("js", new Date());
+        gtag("set", "developer_id.dZTNiMT", true);
+        gtag("config", "GT-WRDFXCC8");
+        /* ]]> */
+    </script>
+    <link rel="icon" href="./images/cropped-20231019-DSCN0114-32x32.jpg" sizes="32x32" /> <!-- TODO: UPDATE ME -->
+    <link rel="icon" href="./images/cropped-20231019-DSCN0114-192x192.jpg" sizes="192x192" /> <!-- TODO: UPDATE ME -->
+    <link rel="apple-touch-icon" href="./images/cropped-20231019-DSCN0114-180x180.jpg" /> <!-- TODO: UPDATE ME -->
+    <link rel="icon" href sizes="32x32">
+    <meta name="msapplication-TileImage"
+        content="https://photography.islayanderson.co.uk/wp-content/uploads/2024/03/cropped-20231019-DSCN0114-270x270.jpg" />
+    <!-- TODO: UPDATE ME -->
+</head>
+
+<body class="" itemscope itemtype="http://schema.org/WebPage">
+    <section id="cover" style="background: #b26df3; display:block;">
+        <img src='/svg/cover.svg' style='width:100%' />
+    </section>
+    <section id="page-1" style="display: block;">
+        <img src='/svg/page1.svg'  style='width:100%' />
+    </section>
+
+    <section id="contents" style="display: block;">
+        <img src='/svg/contents.svg' style='width:100%' />
+    </section>
+
+    <section id="page-3" style="flex-wrap: wrap;">
+        <img src='/svg/page3.svg' style='width:100%' />
+    </section>
+
+    <section id="page-4" style="display: block;">
+        <img src='/svg/page4.svg'  style='width:100%' />
+    </section>
+    <section id="page-5" style="flex-wrap: wrap;">
+        <img src='/svg/page5.svg' style='width:100%'/>
+    </section>
+</body>
+
+</html>
